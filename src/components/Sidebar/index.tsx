e1afa74893fdab2991f6ffe0b61703689135ae5f
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-04-10 15:59:46 +0900

    update snap

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Menu, Icon, Layout } from 'antd';
-import { findIndex } from 'lodash';
 import { Logo } from './style';
 import config from './config';
 
@@ -15,34 +14,14 @@ interface SidebarProps {
 }
 
 export default ({ sidebarIsCollapse }: any) => {
-  let history = useHistory();
   const { langCode } = useContext(I18nContext);
   const [selectedKey, setSelectedKey] = useState('0');
-
-  const handleSelect = ({
-    item,
-    key,
-    keyPath,
-    selectedKeys,
-    domEvent,
-  }: any) => {
-    setSelectedKey(key + '');
-  };
-
-  useEffect(() => {
-    console.log(history);
-  }, [history]);
 
   return (
     <Sider collapsed={sidebarIsCollapse}>
       <Logo>Logo</Logo>
       {/* TODO: change '0' to a sdynamic key in sotre */}
-      <Menu
-        selectedKeys={[selectedKey]}
-        theme="dark"
-        mode="inline"
-        onSelect={handleSelect}
-      >
+      <Menu selectedKeys={[selectedKey]} theme="dark" mode="inline">
         {config.map((item: any, index) => (
           <Menu.Item key={index}>
             <Link to={item.path}>
