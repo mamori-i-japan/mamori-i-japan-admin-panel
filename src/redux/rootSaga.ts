43a07c985088f05ff3c510cc9de641f6433416af
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-04-18 01:20:46 +0900

    change saga module name

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,8 +1,8 @@
 import { all } from 'redux-saga/effects';
 import analyticsSaga from './Analytics/saga';
 import authSaga from './Auth/saga';
-import AdminUserSaga from './AdminUser/saga';
+import adminUserSaga from './AdminUser/saga';
 
 export default function* rootSaga() {
-  yield all([analyticsSaga(), authSaga(), AdminUserSaga()]);
+  yield all([analyticsSaga(), authSaga(), adminUserSaga()]);
 }
