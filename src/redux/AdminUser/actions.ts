31d361d67afbaac1866f8490f6449be4f160a1a4
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-04-23 12:37:35 +0900

    add actions

--- src/redux/AdminUser/actions.ts
+++ src/redux/AdminUser/actions.ts
@@ -2,3 +2,5 @@ import { createAction } from 'redux-actions';
 import actionTypes from './actionTypes';
 
 export const createUserAction = createAction(actionTypes.CREATE_USER);
+
+export const getUsersAction = createAction(actionTypes.GET_USERS);
