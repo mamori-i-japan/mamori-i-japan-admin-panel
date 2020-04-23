4a6cbcfdbcbbe4c91cc4ae76cf6a15d235cf199f
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-04-23 12:32:46 +0900

    add API call function

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,14 +4,34 @@ export const login = () => {
   return http.post('auth/admin/login');
 };
 
-export const postPositive = (data: { phoneNumber: string }) => {
-  return http.post('admins/positives', data);
+export const postAdminUser = (data: { email: string }) => {
+  return http.post('/admins/users', data);
 };
 
 export const getAdminUserList = () => {
   return http.get('admins/users');
 };
 
-export const postAdminUser = (data: { email: string }) => {
-  return http.post('/admins/users', data);
+export const postPositive = (data: { phoneNumber: string }) => {
+  return http.post('admins/positives', data);
 };
+
+// todo: 陽性判定者一覧
+export const getPositives = () => {
+
+};
+
+// todo: 濃厚接触者一覧
+export const getClosedContacts = () => {
+
+};
+
+// todo: お知らせ一覧
+export const getMessages = () => {
+
+};
+
+// todo: お知らせを変更
+export const postMessaage = () => {
+
+}
