b88f0fa0642dfb0578a42c9ac54b46f1a44821a2
ebiryu <ebiryu.zatta@gmail.com>
2020-04-30 18:05:10 +0900

    add "！"

--- src/locales/jaJP.ts
+++ src/locales/jaJP.ts
@@ -30,8 +30,8 @@ export default {
   adminUser: '管理者',
   submitSuccess: '内容を保存しました！',
   deleteSuccess: '内容を削除しました！',
-  loginSuccess: 'ログインしました',
-  logoutSuccess: 'ログアウトしました',
+  loginSuccess: 'ログインしました！',
+  logoutSuccess: 'ログアウトしました！',
   createAdminUserSuccess:
     '管理者を追加しました！追加した管理者にメールを送信しました！',
   adminUserIsExistError:
