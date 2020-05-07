3dc83634baf66532823d746a236db1b8de1771ef
ebiryu <ebiryu.zatta@gmail.com>
2020-05-07 18:32:58 +0900

    fix validate timing

--- src/constants/Validation.ts
+++ src/constants/Validation.ts
@@ -2,9 +2,14 @@ import { langCode } from './index';
 import { langLocales } from '../locales';
 
 export default {
-  required: { required: true, message: langLocales[langCode].isRequired },
+  required: {
+    required: true,
+    message: langLocales[langCode].isRequired,
+    validateTrigger: 'submit',
+  },
   email: {
     type: 'email',
     message: langLocales[langCode].isValidEmail,
+    validateTrigger: 'submit',
   },
 };
