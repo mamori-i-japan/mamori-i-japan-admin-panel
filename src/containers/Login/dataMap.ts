56aed0adfd8190e4bf3d3eba878414eefe9ac150
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-05-13 13:25:28 +0900

    delete useless code

--- src/containers/Login/dataMap.ts
+++ src/containers/Login/dataMap.ts
@@ -7,10 +7,4 @@ export default [
     label: 'email',
     rules: [Validation.required, Validation.email],
   },
-  // {
-  //   name: 'password',
-  //   type: 'password',
-  //   label: 'password',
-  //   rules: [{ required: true, message: 'Please input!' }],
-  // },
 ];
